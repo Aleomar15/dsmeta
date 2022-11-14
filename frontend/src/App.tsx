f54cfc9baf21c876d6a5d894dd307e866705f946
Alexandre O Marinho <alexandreomar0202@gmail.com>
2022-11-14 11:15:48 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,20 @@
+import Header from "./components/Header"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
-  return(
-  <><>
-    </><h1>Olá Mundo!</h1><NotificationButton /></>
-  
+  return (
+    <><>
+    </><Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+
   )
 
 }
