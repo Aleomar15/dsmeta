56a2152524567cca22feed4d1b636d6775054b0c
Alexandre O Marinho <alexandreomar0202@gmail.com>
2022-11-14 11:51:24 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import NotificationButton from "./components/NotificationButton";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   return (
@@ -19,4 +19,4 @@ function App() {
 
 }
 
-export default App
+export default App;
--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 function NotificationButton() {
     return(
         <div className="dsmeta-red-btn">
@@ -8,5 +8,5 @@ function NotificationButton() {
     )
   }
   
-  export default NotificationButton
+  export default NotificationButton;
   
